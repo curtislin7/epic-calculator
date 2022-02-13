041bd9f7dca127ce5272c36e9a680d4db6110cf2
Curtis Lin <curtislin7@gmail.com>
2022-02-13 13:08:21 -0700

    add set calculation to all buttons

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { CalculatorRow, CalculatorButton } from './components';
 
 function App() {
   const [calculation, setCalculation] = React.useState(0)
-
+  console.log(calculation)
   return (
     <div className="App">
       <div className={"calculator-container"}>
@@ -14,29 +14,29 @@ function App() {
           <CalculatorButton text={'7'} setCalculation={setCalculation}/>
           <CalculatorButton text={'8'} setCalculation={setCalculation}/>
           <CalculatorButton text={'9'} setCalculation={setCalculation}/>
-          <CalculatorButton isFunctionButtonText={true} text={'/'}/>
+          <CalculatorButton isFunctionButtonText={true} text={'/'} setCalculation={setCalculation}/>
           <div className={'placeholder'}/>
         </CalculatorRow>
         <CalculatorRow>
           <CalculatorButton text={'4'} setCalculation={setCalculation}/>
           <CalculatorButton text={'5'} setCalculation={setCalculation}/>
           <CalculatorButton text={'6'} setCalculation={setCalculation}/>
-          <CalculatorButton isFunctionButtonText={true} text={'x'}/>
+          <CalculatorButton isFunctionButtonText={true} text={'x'} setCalculation={setCalculation}/>
           <div className={'placeholder'}/>
         </CalculatorRow>
         <CalculatorRow>
           <CalculatorButton text={'1'} setCalculation={setCalculation}/>
           <CalculatorButton text={'2'} setCalculation={setCalculation}/>
           <CalculatorButton text={'3'} setCalculation={setCalculation}/>
-          <CalculatorButton isFunctionButtonText={true} text={'-'}/>
+          <CalculatorButton isFunctionButtonText={true} text={'-'} setCalculation={setCalculation}/>
           <div className={'placeholder'}/>
         </CalculatorRow>
         <CalculatorRow>
           <CalculatorButton text={'0'} setCalculation={setCalculation}/>
           <div className={'placeholder'}/>
           <div className={'placeholder'}/>
-          <CalculatorButton isFunctionButtonText={true} text={'+'}/>
-          <CalculatorButton isFunctionButtonText={true} text={'='}/>
+          <CalculatorButton isFunctionButtonText={true} text={'+'} setCalculation={setCalculation}/>
+          <CalculatorButton isFunctionButtonText={true} text={'='} setCalculation={setCalculation}/>
         </CalculatorRow>
       </div>
     </div>
