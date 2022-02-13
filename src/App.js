0ce031fb77c4738c8503081cc2dfc8fed28b7003
Curtis Lin <curtislin7@gmail.com>
2022-02-13 13:05:10 -0700

    add all buttons

--- src/App.js
+++ src/App.js
@@ -15,23 +15,27 @@ function App() {
           <CalculatorButton text={'8'} setCalculation={setCalculation}/>
           <CalculatorButton text={'9'} setCalculation={setCalculation}/>
           <CalculatorButton isFunctionButtonText={true} text={'/'}/>
+          <div className={'placeholder'}/>
         </CalculatorRow>
         <CalculatorRow>
           <CalculatorButton text={'4'} setCalculation={setCalculation}/>
           <CalculatorButton text={'5'} setCalculation={setCalculation}/>
           <CalculatorButton text={'6'} setCalculation={setCalculation}/>
           <CalculatorButton isFunctionButtonText={true} text={'x'}/>
+          <div className={'placeholder'}/>
         </CalculatorRow>
         <CalculatorRow>
           <CalculatorButton text={'1'} setCalculation={setCalculation}/>
           <CalculatorButton text={'2'} setCalculation={setCalculation}/>
           <CalculatorButton text={'3'} setCalculation={setCalculation}/>
           <CalculatorButton isFunctionButtonText={true} text={'-'}/>
+          <div className={'placeholder'}/>
         </CalculatorRow>
         <CalculatorRow>
-          <div className={'placeholder'}/>
           <CalculatorButton text={'0'} setCalculation={setCalculation}/>
           <div className={'placeholder'}/>
+          <div className={'placeholder'}/>
+          <CalculatorButton isFunctionButtonText={true} text={'+'}/>
           <CalculatorButton isFunctionButtonText={true} text={'='}/>
         </CalculatorRow>
       </div>
