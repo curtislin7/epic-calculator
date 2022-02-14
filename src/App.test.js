2fd652f3301def0510867dc4dc9328467f726c0d
Curtis Lin <curtislin7@gmail.com>
2022-02-13 22:28:08 -0700

    add tests with assertions and different page

--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,15 @@
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import App from './App';
-import { MemoryRouter } from 'react-router-dom'
-
-const renderWithRouter = (ui, {route = '/'} = {}) => {
-  window.history.pushState({}, 'Test page', route)
-  return render(ui, {wrapper: MemoryRouter})
-}
+import { renderWithRouter } from './testUtil'
 
 describe('main calculator page', () => {
   it('renders the important title', () => {
-    render(<App />, {wrapper: MemoryRouter});
-    
-    const linkElement = screen.getByText(/HARD CORE CALCULATOR./i);
+    renderWithRouter(<App />, {route: '/calculator'});
+    expect(screen.getByText(/HARD CORE CALCULATOR./i)).toBeInTheDocument()
+  })
+
+  it('renders the important text in the about page', () => {
+    renderWithRouter(<App />, {route: '/about'});
+    expect(screen.getByText(/this app is super ready for production/i)).toBeInTheDocument()
   })
 });
