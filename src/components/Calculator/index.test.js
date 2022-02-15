a79920c9ffd8fe64247fabb62e108fb400289936
Curtis Lin <curtislin7@gmail.com>
2022-02-14 21:53:18 -0700

    add addition test in calculator page using multiple button presses

--- src/components/Calculator/index.test.js
+++ src/components/Calculator/index.test.js
@@ -2,12 +2,23 @@ import { screen, fireEvent, waitFor } from '@testing-library/react';
 import { Calculator } from './index.js';
 import { renderWithRouter } from 'testUtil.js'
 
-describe('about page', () => {
+describe('calculator', () => {
   it('displays number after clicking a button', async () => {
     renderWithRouter(<Calculator/>, {route: '/calculator'});
 
     fireEvent.click(screen.getByText("9"))
     await waitFor(() => expect(screen.getAllByText("9")).toHaveLength(2))
+  })
 
+  it('adds numbers', async () => {
+    renderWithRouter(<Calculator/>, {route: '/calculator'});
+
+    const numberNineButton = screen.getByText("9")
+    fireEvent.click(numberNineButton)
+    fireEvent.click(screen.getByText("+"))
+    fireEvent.click(numberNineButton)
+    fireEvent.click(screen.getByText("="))
+
+    await waitFor(() => expect(screen.getByText("18")).toBeInTheDocument)
   })
 });
